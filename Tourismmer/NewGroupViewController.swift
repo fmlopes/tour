2ee9ee9d588a972b3c9cc3edd834f1a77bdb4a3e
superare <felipe@superaremedia.com.br>
2014-12-22 20:09:11 -0200

    m

--- Tourismmer/NewGroupViewController.swift
+++ Tourismmer/NewGroupViewController.swift
@@ -8,19 +8,21 @@
 
 import Foundation
 
-class NewGroupViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate, GoogleServiceProtocol, APIProtocol  {
+class NewGroupViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate, GoogleServiceProtocol, APIProtocol, UITableViewDelegate, UITableViewDataSource  {
     
     var pickerViewArray:[TripType] = [TripType.Business, TripType.ExchangeProgram, TripType.Tourism, TripType.Excursion]
     var filteredCities = [String]()
     var cities = [String]()
     lazy var googleService:GoogleService = GoogleService(delegate: self)
     lazy var api:API = API(delegate: self)
+    @IBOutlet weak var tableViewCities: UITableView!
     
     @IBOutlet weak var pickerViewTripType: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.tableViewCities.hidden = true
     }
     
     @IBAction func create(sender: AnyObject) {
@@ -38,6 +40,22 @@ class NewGroupViewController: UIViewController, UIPickerViewDataSource, UIPicker
         return pickerViewArray[row].rawValue
     }
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableViewCities.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        cell.textLabel?.text = filteredCities[indexPath.row]
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredCities.count
+    }
+    
     func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
         
         self.filterContentForSearchText(searchString)
@@ -51,7 +69,8 @@ class NewGroupViewController: UIViewController, UIPickerViewDataSource, UIPicker
     }
     
     func filterContentForSearchText(searchText: String) {
-        googleService.HTTPGet("/maps/api/place/autocomplete/json?input=\(searchText)&key=&language=pt_BR&types=(cities)")
+        googleService.HTTPGet("/maps/api/place/autocomplete/json?input=\(searchText)&key=AIzaSyC47pgYWvg_sVs5cs2_UXKoIsDPQTo8z7A&language=pt_BR&types=(cities)")
+        self.tableViewCities.hidden = false
     }
     
     func didReceiveGoogleResults(results: NSDictionary) {
